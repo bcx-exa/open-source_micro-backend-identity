6d6de99a9b724730911a58c1a0951b4c527f350a
Bradley Henderson <bradleyhenderson22@gmail.com>
2020-10-07 10:02:08 +0200

    add tests

--- tests/authentication.test.ts
+++ tests/authentication.test.ts
@@ -1,5 +1,37 @@
 import { AuthenticationService } from '../src/services/authentication';
 import { SignUp } from '../src/types/authentication';
+import { Server } from '../src/server'
+import request from "supertest";
+const expressApp = new Server();
+expressApp.Start();
+
+
+describe("User Sign up", () => {
+  test(`Password Policy Validation`, async () => {
+    const res = await request(expressApp.app)
+      .post('/auth/signup')
+      .send({
+        "preferred_username": "noPassword@gmail.com",
+        "password": "123",
+        "given_name": "foo",
+        "family_name": "bar"
+      })
+    expect(res.statusCode).toEqual(406)
+    expect(res.body).toHaveProperty('name')
+  }, 30000);
+  test(`Valid Signup`, async () => {
+    const res = await request(expressApp.app)
+      .post('/auth/signup')
+      .send({
+        "preferred_username": "test@gmail.com",
+        "password": "Password@1",
+        "given_name": "foo",
+        "family_name": "bar"
+      })
+    expect(res.statusCode).toEqual(409)
+  }, 30000);
+})
+
 
 
 test("Password Policy Validation", async () => {
