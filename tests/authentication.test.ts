fe8db1d857412e705d669a879c5a26d90e0abb2b
RobLouw <robertllouw@gmail.com>
2020-10-07 14:05:59 +0200

    Added env.local

--- tests/authentication.test.ts
+++ tests/authentication.test.ts
@@ -11,45 +11,45 @@ try {
 }
 
 
-describe('Post Endpoints', () => {
-  it('should create a new post', async () => {
-    const res = await request(expressApp.app)
-      .get('/user');
-    
-    console.log(res);
-    
-    expect(res.statusCode).toEqual(200)
-    //expect(res.body).toHaveProperty('post')
-  })
-})
+// describe('Post Endpoints', () => {
+//   it('should create a new post', async () => {
+//     const res = await request(expressApp.app)
+//       .get('/user');
 
-// describe("User Sign up", () => {
-//   test(`Password Policy Validation`, async () => {
-//     const res = await request(expressApp.app)
-//       .post('/auth/signup')
-//       .send({
-//         "preferred_username": "noPassword@gmail.com",
-//         "password": "123",
-//         "accepted_legal_version": "1.0.1",
-//         "given_name": "foo",
-//         "family_name": "bar"
-//       })
-//     expect(res.statusCode).toEqual(406)
-//     expect(res.body).toHaveProperty('name')
-//   }, 30000);
-//   test(`Valid Signup`, async () => {
-//     const res = await request(expressApp.app)
-//       .post('/auth/signup')
-//       .send({
-//         "preferred_username": "test@gmail.com",
-//         "accepted_legal_version": "1.0.1",
-//         "password": "Password@1",
-//         "given_name": "foo",
-//         "family_name": "bar"
-//       })
-//     expect(res.statusCode).toEqual(409)
-//   }, 30000);
+//     console.log(res);
+
+//     expect(res.statusCode).toEqual(200)
+//     //expect(res.body).toHaveProperty('post')
+//   })
 // })
+
+describe("User Sign up", () => {
+  test(`Password Policy Validation`, async () => {
+    const res = await request(expressApp.app)
+      .post('/auth/signup')
+      .send({
+        "preferred_username": "noPassword@gmail.com",
+        "password": "123",
+        "accepted_legal_version": "1.0.1",
+        "given_name": "foo",
+        "family_name": "bar"
+      })
+    expect(res.statusCode).toEqual(406)
+    expect(res.body).toHaveProperty('name')
+  }, 30000);
+  test(`Valid Signup`, async () => {
+    const res = await request(expressApp.app)
+      .post('/auth/signup')
+      .send({
+        "preferred_username": "test@gmail.com",
+        "accepted_legal_version": "1.0.1",
+        "password": "Password@1",
+        "given_name": "foo",
+        "family_name": "bar"
+      })
+    expect(res.statusCode).toEqual(409)
+  }, 30000);
+})
 
 
 
