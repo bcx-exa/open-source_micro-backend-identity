f02704fd8c9a474938dc3bddffcaa6441cc97b3d
Bradley Henderson <bradleyhenderson22@gmail.com>
2020-10-20 08:41:22 +0200

    u

--- tests/users.test.ts
+++ tests/users.test.ts
@@ -5,27 +5,30 @@ describe("Users", () => {
     it('Get Users', async () => {
         const res = await agent.get("/user").set('Authorization', 'Bearer ' + token)
     });
-    it('Crreate user', async () => {
-        const res = await agent.post("/user").set('Authorization', 'Bearer ' + token)
-            .send({
-                "user_id": "string",
-                "preferred_username": "string",
-                "password": "string",
-                "phone_number": "+27",
-                "email": "string",
-                "address": "string",
-                "locale": "string",
-                "birthdate": "2020-10-19T06:38:37.613Z",
-                "name": "Test User",
-                "given_name": "Test",
-                "family_name": "User",
-                "accepted_legal_version": "v1",
-                "nickname": "Test",
-                "gender": "Other",
-                "picture": "None"
-            })
-        expect(res.statusCode).toEqual(200)
-    });
+    // it('Create user', async () => {
+    //     const res = await agent.post("/user").set('Authorization', 'Bearer ' + token)
+    //         .send({
+    //             "user_id": "string",
+    //             "preferred_username": "newAccount@gmail.com",
+    //             "password": "Password@1",
+    //             "phone_number": "+27721234567",
+    //             "email": "newAccount@gmail.com",
+    //             "address": "1 Main Road",
+    //             "locale": "en",
+    //             "birthdate": "2020-10-19T06:38:37.613Z",
+    //             "name": "Test User",
+    //             "given_name": "Test",
+    //             "family_name": "User",
+    //             "accepted_legal_version": "v1",
+    //             "nickname": "Test",
+    //             "gender": "Other",
+    //             "picture": "None"
+    //         })
+    //     console.log("user", res)
+    //     expect(res.statusCode).toEqual(200)
+    //     expect(res.body).toHaveProperty('message')
+    //     expect(res.body.message).toEqual('Success')
+    // });
 
 
 
